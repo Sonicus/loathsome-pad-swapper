2714ef53e2487510df7f41c661d01178f7ec8fa4
Sonicus <jere.nevalainen@gmail.com>
2022-10-20 18:04:05 +0300

    Pass controller state to virtual controller

--- LoathsomePadSwapper/Program.cs
+++ LoathsomePadSwapper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Nefarius.ViGEm.Client;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
 using SharpDX.XInput;
 
 Console.WriteLine("Starting Loathsome Pad Swapper...");
@@ -53,17 +54,57 @@ virtualController.Connect();
 
 Console.WriteLine("Virtual controller initialized");
 
-var previousState1 = _controller1.GetState();
-while (_controller1.IsConnected)
+var activeController = _controller1;
+Console.WriteLine($"{(ushort)activeController.UserIndex + 1} is the active controller");
+var previousState = _controller1.GetState();
+while (activeController.IsConnected)
 {
     if (IsKeyPressed(ConsoleKey.Escape))
     {
         break;
     }
-    var state1 = _controller1.GetState();
-    if (previousState1.PacketNumber != state1.PacketNumber)
-        Console.WriteLine(state1.Gamepad);
-    previousState1 = state1;
+    if (IsKeyPressed(ConsoleKey.Spacebar))
+    {
+        activeController = activeController.UserIndex == _controller1.UserIndex ? _controller2 : _controller1;
+        Console.WriteLine($"{(ushort)activeController.UserIndex + 1} is the active controller");
+    }
+
+    var state = activeController.GetState();
+    if (previousState.PacketNumber != state.PacketNumber)
+    {
+        PassStateToVirtualController(state.Gamepad);
+    }
+    previousState = state;
+}
+
+void PassStateToVirtualController(Gamepad gamepad)
+{
+    virtualController.SetButtonState(Xbox360Button.A, gamepad.Buttons.HasFlag(GamepadButtonFlags.A));
+    virtualController.SetButtonState(Xbox360Button.B, gamepad.Buttons.HasFlag(GamepadButtonFlags.B));
+    virtualController.SetButtonState(Xbox360Button.X, gamepad.Buttons.HasFlag(GamepadButtonFlags.X));
+    virtualController.SetButtonState(Xbox360Button.Y, gamepad.Buttons.HasFlag(GamepadButtonFlags.Y));
+
+    virtualController.SetButtonState(Xbox360Button.Start, gamepad.Buttons.HasFlag(GamepadButtonFlags.Start));
+    virtualController.SetButtonState(Xbox360Button.Back, gamepad.Buttons.HasFlag(GamepadButtonFlags.Back));
+
+    virtualController.SetButtonState(Xbox360Button.LeftShoulder, gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftShoulder));
+    virtualController.SetButtonState(Xbox360Button.RightShoulder, gamepad.Buttons.HasFlag(GamepadButtonFlags.RightShoulder));
+
+    virtualController.SetButtonState(Xbox360Button.LeftThumb, gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb));
+    virtualController.SetButtonState(Xbox360Button.RightThumb, gamepad.Buttons.HasFlag(GamepadButtonFlags.RightThumb));
+
+    virtualController.SetButtonState(Xbox360Button.Left, gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft));
+    virtualController.SetButtonState(Xbox360Button.Right, gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadRight));
+    virtualController.SetButtonState(Xbox360Button.Up, gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp));
+    virtualController.SetButtonState(Xbox360Button.Down, gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown));
+
+    virtualController.SetAxisValue(Xbox360Axis.LeftThumbX, gamepad.LeftThumbX);
+    virtualController.SetAxisValue(Xbox360Axis.LeftThumbY, gamepad.LeftThumbY);
+    virtualController.SetAxisValue(Xbox360Axis.RightThumbX, gamepad.RightThumbX);
+    virtualController.SetAxisValue(Xbox360Axis.RightThumbY, gamepad.RightThumbY);
+
+    virtualController.SetSliderValue(Xbox360Slider.LeftTrigger, gamepad.LeftTrigger);
+    virtualController.SetSliderValue(Xbox360Slider.RightTrigger, gamepad.RightTrigger);
 }
 
 
