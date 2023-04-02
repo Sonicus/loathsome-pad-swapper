0110f30f7694b4cee3fca3489067bfc92c4ee966
Sonicus <jere.nevalainen@gmail.com>
2023-04-02 12:38:05 +0300

    Moving to MVVM

--- LoathsomePadSwapper/MainWindow.xaml.cs
+++ LoathsomePadSwapper/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using SharpDX.XInput;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,72 +6,27 @@ namespace LoathsomePadSwapper
 {
     public partial class MainWindow : Window
     {
-        Controller? Pad1 { get; set; }
-        Controller? Pad2 { get; set; }
-        ObservableCollection<Controller> Controllers { get; set; } = new ObservableCollection<Controller>();
-        bool AreButtonsEnabled { get; set; } = true;
+        PadSwapperViewModel ViewModel { get; }
 
         public MainWindow()
         {
+            ViewModel = new PadSwapperViewModel();
+            DataContext = ViewModel;
             InitializeComponent();
-            InitControllers();
-            padList.ItemsSource = Controllers;
-            pad1Button.DataContext = AreButtonsEnabled;
-            pad2Button.DataContext = AreButtonsEnabled;
         }
 
-        private void InitControllers()
-        {
-            for (int i = 0; i <= 3; i++)
-            {
-                Controllers.Add(new Controller((UserIndex)i));
-            }
-        }
-
-        private void RefreshPads_Click(object sender, RoutedEventArgs e) => padList.Items.Refresh();
-
-        private async void AssignControllerButton_Click(object sender, RoutedEventArgs e)
+        private void AssignControllerButton_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
             var controllerIndex = Int32.Parse((string)button!.Tag);
             Debug.WriteLine($"Assigning controller {controllerIndex}");
 
-            AreButtonsEnabled = false;
-            button.Content = "Press A on a controller";
-            await AssignController(controllerIndex);
-            AreButtonsEnabled = true;
-
-            button.Content = "OK";
-            Debug.WriteLine($"Controller {controllerIndex} assigned");
-        }
-
-        Task AssignController(int index)
-        {
-            return Task.Run(() =>
+            if( controllerIndex == 1 ) {
+                ViewModel.Pad2ButtonEnabled = !ViewModel.Pad2ButtonEnabled;
+            } else
             {
-                while (true)
-                {
-                    foreach (var controller in Controllers)
-                    {
-                        if (controller.IsConnected && controller.GetState().Gamepad.Buttons == GamepadButtonFlags.A)
-                        {
-                            if (index == 1)
-                            {
-                                Pad1 = controller;
-                            }
-                            else
-                            {
-                                Pad2 = controller;
-                            }
-                            return;
-                        }
-                    }
-                    Thread.Sleep(10);
-                }
-            });
-
+                ViewModel.Pad1ButtonEnabled = !ViewModel.Pad1ButtonEnabled;
+            }
         }
-
-
     }
 }
---
+++ LoathsomePadSwapper/PadSwapperViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LoathsomePadSwapper
+{
+    internal class PadSwapperViewModel : ViewModelBase
+    {
+        private bool _pad1ButtonEnabled;
+        private bool _pad2ButtonEnabled;
+
+        public bool Pad1ButtonEnabled { get => _pad1ButtonEnabled; set => SetProperty(ref _pad1ButtonEnabled, value); }
+        public bool Pad2ButtonEnabled { get => _pad2ButtonEnabled; set => SetProperty(ref _pad2ButtonEnabled, value); }
+
+        public PadSwapperViewModel()
+        {
+            Pad1ButtonEnabled = true;
+            Pad2ButtonEnabled = true;
+        }
+    }
+}
